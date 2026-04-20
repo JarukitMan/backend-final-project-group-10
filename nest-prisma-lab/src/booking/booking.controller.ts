9e3552a6c8e947b0c08b636489723c6ccf5faf30
JarukitMan <pleaseletmeinijustwanttoemail@gmail.com>
2026-04-20 17:21:36 +0700

    feat: initial implementation. Untested. 	new file:   src/booking/booking.controller.spec.ts 	new file:   src/booking/booking.controller.ts 	new file:   src/booking/booking.module.ts 	new file:   src/booking/booking.service.spec.ts 	new file:   src/booking/booking.service.ts 	new file:   src/booking/dto/book.dto.ts 	new file:   src/booking/dto/edit-booking.dto.ts 	new file:   src/booking/dto/unbook.dto.ts

---
+++ nest-prisma-lab/src/booking/booking.controller.ts
@@ -0,0 +1,59 @@
+import { Body, Controller, Get, Post, Req, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Role, Roles } from 'src/roles/roles.decorator';
+import { BookDto } from './dto/book.dto';
+import { BookingService } from './booking.service';
+// Err, I did it this way because of the linter.
+import * as e from 'express';
+import { UnbookDto } from './dto/unbook.dto';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { RolesGuard } from 'src/roles/roles.guard';
+import { ApiTags } from '@nestjs/swagger';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { EditBookingDto } from './dto/edit-booking.dto';
+
+@Controller('booking')
+@UseInterceptors(CacheInterceptor)
+@ApiTags('booking')
+export class BookingController {
+  constructor(private service: BookingService) {}
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User, Role.Admin)
+  @Post('book')
+  book(@Req() req: e.Request, @Body() dto: BookDto) {
+    return this.service.book(req, dto)
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Post('unbook')
+  @Roles(Role.User, Role.Admin)
+  unbook(@Req() req: e.Request, @Body() dto: UnbookDto) {
+    return this.service.unbook(req, dto)
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User, Role.Admin)
+  @Get('my-bookings')
+  my_bookings(@Req() req: e.Request) {
+    return this.service.my_bookings(req)
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get('all-bookings')
+  all_bookings() {
+    return this.service.all_bookings()
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Post('edit-booking')
+  edit_booking(dto: EditBookingDto) {
+    return this.service.edit_booking(dto)
+  }
+}
+
+/*
+- Logged-in users can create and manage their own bookings with validation of dates and prevention of double booking.
+- Booking statuses: Pending (default), Approved, Cancelled, Paid, with Admin ability to view and update any booking.
+*/
