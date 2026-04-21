61bc48c8ad03b8b964d97a7def9bedd2ee16bcb9
pearlispearl <rawisarachantra@gmail.com>
2026-04-22 02:37:55 +0700

    refactor: update modules to support dependency overrides for tests

--- nest-prisma-lab/src/rooms/rooms.module.ts
+++ nest-prisma-lab/src/rooms/rooms.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
